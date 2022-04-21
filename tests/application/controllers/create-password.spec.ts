eae353514735fbdc8156afca2823b7b1a412b60e
Giovani Farias <riccog.25@gmail.com>
2022-04-21 19:18:23 -0300

    test: ensure CreatePasswordController returns 400 if no title is provided

--- tests/application/controllers/create-password.spec.ts
+++ tests/application/controllers/create-password.spec.ts
@@ -1,6 +1,7 @@
 type HttpRequest = {
   body: {
     password?: string
+    title?: string
   }
 }
 
@@ -19,16 +20,28 @@ class MissingParamError extends Error {
 
 class CreatePasswordController {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (httpRequest.body.password === undefined || httpRequest.body.password === '' || httpRequest.body.password === null) {
+      return {
+        body: new MissingParamError('password'),
+        statusCode: 400
+      }
+    }
+
     return {
-      body: new MissingParamError('password'),
+      body: new MissingParamError('title'),
       statusCode: 400
     }
   }
 }
 
 describe('CreatePasswordController', () => {
+  let sut: CreatePasswordController
+
+  beforeEach(() => {
+    sut = new CreatePasswordController()
+  })
+
   it('should return 400 if no password is provided', async () => {
-    const sut = new CreatePasswordController()
     const httpRequest = {
       body: {
         password: undefined
@@ -39,5 +52,19 @@ describe('CreatePasswordController', () => {
 
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new MissingParamError('password'))
+  })
+
+  it('should return 400 if no title is provided', async () => {
+    const httpRequest = {
+      body: {
+        password: 'any_password',
+        title: undefined
+      }
+    }
+
+    const response = await sut.handle(httpRequest)
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('title'))
   })
 })
