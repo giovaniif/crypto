b25bdb505b7eb5b41330654282e9d5f9e72ecbc3
Giovani Farias <riccog.25@gmail.com>
2022-04-24 21:39:33 -0300

    feat: add create password routes

--- src/main/routes/password.ts
+++ src/main/routes/password.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 
+import { adaptExpressRoute } from '@/main/adapters'
+import { makeCreatePasswordController } from '@/main/factories/controllers'
+
 export default (router: Router): void => {
-  router.post('/passwords', (req, res) => {
-    res.send('Hello World!')
-  })
+  router.post('/passwords', adaptExpressRoute(makeCreatePasswordController()))
 }
